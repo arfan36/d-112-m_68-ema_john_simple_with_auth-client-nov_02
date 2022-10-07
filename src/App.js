c0fe6452eadda21c36e3d032f331e12f15562ee6
arfan36 <arfanur36@gmail.com>
2022-10-07 11:18:30 +0600

    53-2 Recap React Router setup and intro to order review page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Inventory from './components/Inventory/Inventory';
+import Orders from './components/Orders/Orders';
+import Shop from './components/Shop/Shop';
 import Main from './layouts/Main';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Main></Main>
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Shop></Shop>
+        },
+        {
+          path: '/orders',
+          element: <Orders></Orders>
+        },
+        {
+          path: '/inventory',
+          element: <Inventory></Inventory>
+        },
+        {
+          path: '/about',
+          element: <About></About>
+        }
+      ]
     }
   ]);
   return (
