3d2296106c1e16abc63e43acfaf281ca3e5f8d6d
arfan36 <arfanur36@gmail.com>
2022-11-06 06:21:32 +0600

    68-3 Introduction to pagination and pagination concept

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -19,7 +19,7 @@ const Shop = () => {
         const storedCart = getStoredCart();
         const savedCart = [];
         for (const id in storedCart) {
-            const addedProduct = products.find(product => product.id === id);
+            const addedProduct = products.find(product => product._id === id);
             if (addedProduct) {
                 const quantity = storedCart[id];
                 addedProduct.quantity = quantity;
@@ -32,19 +32,19 @@ const Shop = () => {
     const handleAddToCart = (selectedProduct) => {
         // console.log(selectedProduct);
         let newCart = [];
-        const exists = cart.find(product => product.id === selectedProduct.id);
+        const exists = cart.find(product => product._id === selectedProduct._id);
         if (!exists) {
             selectedProduct.quantity = 1;
             newCart = [...cart, selectedProduct];
         }
         else {
-            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            const rest = cart.filter(product => product._id !== selectedProduct._id);
             exists.quantity = exists.quantity + 1;
             newCart = [...rest, exists];
         }
         // const newCart = [...cart, selectedProduct];
         setCart(newCart);
-        addToDb(selectedProduct.id);
+        addToDb(selectedProduct._id);
     };
 
     return (
@@ -52,7 +52,7 @@ const Shop = () => {
             <div className="product-container">
                 {
                     products.map(product => <Product
-                        key={product.id}
+                        key={product._id}
                         product={product}
                         handleAddToCart={handleAddToCart}
                     ></Product>)
--- src/loaders/productAndCartLoader.js
+++ src/loaders/productAndCartLoader.js
@@ -2,14 +2,14 @@ import { getStoredCart } from "../utilities/fakedb";
 
 export const productAndCartLoader = async () => {
     // get Product
-    const productData = await fetch('products.json');
+    const productData = await fetch('http://localhost:5000/products');
     const products = await productData.json();
 
     // get cart
     const savedCart = getStoredCart();
     const initialCart = [];
     for (const id in savedCart) {
-        const addedProduct = products.find(product => product.id === id);
+        const addedProduct = products.find(product => product._id === id);
         if (addedProduct) {
             const quantity = savedCart[id];
             addedProduct.quantity = quantity;
